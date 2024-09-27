dfb6e8e7de668766222855d212dfee65d4512a51
Dineshkumar14498 <dineshvlr9500@gmail.com>
2024-09-27 16:17:51 +0530

    update

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,18 +1,19 @@
-const mongoose = require("mongoose")
+const mongoose = require ("mongoose")
 
 //const mongoURI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/myDatabase?retryWrites=true&w=majority";
 
 
 const connectDb = async () => {
     try {
-        await mongoose.connect (process.env.CONNECTION_STRING,)
-        console.log("MongoDB connected successfully");
-    } 
-    
-    catch (error) {
-        console.error("MongoDB connection error", error);
-        process.exit(1);
+      await mongoose.connect(process.env.MONGO_URI, {
+       
+      });
+      console.log('MongoDB connected');
+    } catch (err) {
+      console.error('MongoDB connection error:', err);
+      process.exit(1);
     }
-};
-
+  };
+  
+  connectDb();
 module.exports = connectDb
